19f322555eb26ab307d87c54a6f0e3082c612ffd
belehx <161518280+belehx@users.noreply.github.com>
2024-02-27 20:37:47 -0300

    Add files via upload

---
+++ exerc5/exerc1/exerc1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerc1
+
+    {
+        internal class Program
+        {
+
+            static void Main(string[] args)
+            {
+                int n1, n2, SOMA;
+
+                Console.WriteLine("Digite um número: ");
+                n1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite outro número: ");
+                n2 = int.Parse(Console.ReadLine());
+
+                SOMA = n1 + n2;
+
+                if (SOMA > 10) 
+                {
+                    Console.WriteLine(SOMA + 5);
+                } else
+                {
+                    Console.WriteLine(SOMA - 7);
+                }
+                Console.ReadKey(); 
+
+            }
+        }
+    }
+
+
---
+++ exerc5/exerc5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerc5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Digite os valores dos lados do triângulo:");
+            Console.Write("lado A: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("lado B: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("lado C: ");
+            c = int.Parse(Console.ReadLine());
+
+
+            if (a < b + c && b < a + c && c < a + b)
+            {
+
+                if (a == b && b == c)
+                {
+                    Console.WriteLine("Triângulo equilátero.");
+                }
+                else if (a == b || a == c || b == c)
+                {
+                    Console.WriteLine("Triângulo isósceles.");
+                }
+                else
+                {
+                    Console.WriteLine("Triângulo escaleno.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Os valores não formam um triângulo.");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
